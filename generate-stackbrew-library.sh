2877164456c2cee05b061363f14589e4f77dd7f1
Shawn Ma <qsma@thoughtworks.com>
2015-10-28 17:43:36 +0800

    update

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,7 +3,7 @@ set -e
 
 declare -A aliases
 aliases=(
-	[1.7]='1.7 latest'
+	[1.7]='1 latest'
 )
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
@@ -12,14 +12,9 @@ versions=( */ )
 versions=( "${versions[@]%/}" )
 url='git@github.com:baselibrary/docker-elasticsearch.git'
 
-echo '# Maintainer: ShawnMa <qsma@thoughtworks.com>'
-
 for version in "${versions[@]}"; do
-  echo $version
   commit="$(cd "$version" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-  fullVersion="$(grep -m1 'ENV ELASTICSEARCH_VERSION' "$version/Dockerfile" | cut -d' ' -f3 | sed 's/~/-/g')"
-
-  echo $fullVersion
+  fullVersion="$(grep -m1 'ENV ELASTICSEARCH_VERSION' "$version/Dockerfile" | cut -d' ' -f3)"
 
   versionAliases=()
 	while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
