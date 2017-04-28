39917f5461d8c5763ab1f220e613cfaca0dc8641
Shawn Ma <qsma@thoughtworks.com>
2017-04-28 13:48:18 +0800

    update

--- update.sh
+++ update.sh
@@ -10,12 +10,14 @@ fi
 versions=( "${versions[@]%/}" )
 
 
-for version in "${versions[@]}"; do	
-  if [ "${version%%.*}" -ge 2 ]; then
-  	repoVersion="${version%%.*}.x"
-  	repoPackage="http://packages.elastic.co/elasticsearch/${repoVersion}/debian/dists/stable/main/binary-amd64/Packages.gz"
+for version in "${versions[@]}"; do
+  if [ "${version%%.*}" -eq 2 ]; then
+  	repoPackage="http://packages.elastic.co/elasticsearch/2.x/debian/dists/stable/main/binary-amd64/Packages.gz"
   	fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
-  else
+  elif [ "${version%%.*}" -eq 5 ]; then
+  	repoPackage="http://artifacts.elastic.co/packages/5.x/apt/dists/stable/main/binary-amd64/Packages.gz"
+  	fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
+	else
   	repoVersion="${version}"
   	repoPackage="http://packages.elastic.co/elasticsearch/${repoVersion}/debian/dists/stable/main/binary-amd64/Packages.gz"
   	fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
