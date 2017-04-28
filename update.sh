7cb350c13ae1630568c11875715c2ae1312799bc
Shawn Ma <qsma@thoughtworks.com>
2017-04-28 14:01:14 +0800

    update

--- update.sh
+++ update.sh
@@ -12,15 +12,15 @@ versions=( "${versions[@]%/}" )
 
 for version in "${versions[@]}"; do
   if [ "${version%%.*}" -eq 2 ]; then
-  	repoPackage="http://packages.elastic.co/elasticsearch/2.x/debian/dists/stable/main/binary-amd64/Packages.gz"
-  	fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
+  	repoPackage="http://packages.elastic.co/elasticsearch/2.x"
+  	fullVersion="$(curl -fsSL "${repoPackage}/debian/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
   elif [ "${version%%.*}" -eq 5 ]; then
-  	repoPackage="http://artifacts.elastic.co/packages/5.x/apt/dists/stable/main/binary-amd64/Packages.gz"
-  	fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
+  	repoPackage="http://artifacts.elastic.co/packages/5.x"
+  	fullVersion="$(curl -fsSL "${repoPackage}/apt/dists/stable/main/binary-amd64/Packages.gz"    | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
 	else
   	repoVersion="${version}"
-  	repoPackage="http://packages.elastic.co/elasticsearch/${repoVersion}/debian/dists/stable/main/binary-amd64/Packages.gz"
-  	fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
+  	repoPackage="http://packages.elastic.co/elasticsearch/${repoVersion}"
+  	fullVersion="$(curl -fsSL "${repoPackage}/debian/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname="elasticsearch" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
   fi
 	(
 		set -x
@@ -28,7 +28,7 @@ for version in "${versions[@]}"; do
 		sed '
 			s/%%ELASTICSEARCH_MAJOR%%/'"$version"'/g;
 			s/%%ELASTICSEARCH_VERSION%%/'"$fullVersion"'/g;
-			s!%%REPO%%!'"$repoVersion"'!g;
+			s!%%ELASTICSEARCH_REPO%%!'"$repoPackage"'!g;
 		' Dockerfile.template > "$version/Dockerfile"
 	)
 done
